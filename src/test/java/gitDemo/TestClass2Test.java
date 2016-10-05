edaf29515bd8b26654520a4ad7fff26e1641178a
Veok <p.lelental@gmail.com>
2016-10-05 14:51:37 +0200

    first

---
+++ src/test/java/gitDemo/TestClass2Test.java
@@ -0,0 +1,16 @@
+package gitDemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by L on 05.10.2016.
+ */
+public class TestClass2Test {
+
+    @Test
+    public void test() {
+        fail("s13784");
+
+}}
